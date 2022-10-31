0f8e688482ce8fe4a5ab8e71a4590ed8d1fd150b
Faisal <faisalhfz10@gmail.com>
2022-10-31 21:45:09 +0700

    update jawaban soal 1

--- day5/tugas-week1/soal1/main.go
+++ day5/tugas-week1/soal1/main.go
@@ -1,38 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func main() {
-	prices := map[string]int{}
+func buyMaxItems(menu map[string]int, budget int) []string {
+	items := sortFromCheapest(menu)
+	total := 0
+	buy := make([]string, 0)
 
-	prices["Benih Lele"] = 50000
-	prices["Pakan lele cap menara"] = 25000
-	prices["Probiotik A"] = 75000
-	prices["Probiotik Nila B"] = 10000
-	prices["Pakan Nila"] = 20000
-	prices["Benih Nila A"] = 20000
-	prices["Cupang"] = 5000
-	prices["Benih Nila B"] = 30000
-	prices["Benih Cupang"] = 10000
-	prices["Probiotik B"] = 10000
+	for _, item := range items {
+		if total+menu[item] > budget {
+			break
+		}
 
-	fmt.Println("-----------------------------------------------")
+		total += menu[item]
+		buy = append(buy, item)
+	}
 
-	// a
-	fmt.Println("Total produk dengan harga dibawah  Rp 100.000 :")
+	return buy
+}
 
-	items := make([]string, len(prices))
+func sortFromCheapest(menu map[string]int) []string {
+	items := make([]string, 0)
 
-	i := 0
-	for item, price := range prices {
-		items[i] = item
-		i++
+	for item, price := range menu {
+		items = append(items, item)
 		_ = price
 	}
 
 	for i := 0; i < len(items)-1; i++ {
 		for j := 0; j < len(items)-1; j++ {
-			if prices[items[j]] > prices[items[j+1]] {
+			if menu[items[j]] > menu[items[j+1]] {
 				temp := items[j+1]
 				items[j+1] = items[j]
 				items[j] = temp
@@ -40,29 +40,83 @@ func main() {
 		}
 	}
 
-	total := 0
-	for _, item := range items {
-		total += prices[item]
-		if total > 100000 {
-			break
+	return items
+}
+
+func cheapestItem(menu map[string]int) string {
+	min := math.MaxInt64
+	var cheapestItem string
+	for item, price := range menu {
+		if price < min {
+			min = price
+			cheapestItem = item
 		}
-		fmt.Println(item, "-", prices[item])
+	}
+	return cheapestItem
+}
+
+func highestItem(menu map[string]int) string {
+	max := 0
+	var highestItem string
+	for item, price := range menu {
+		if price > max {
+			max = price
+			highestItem = item
+		}
+	}
+	return highestItem
+}
+
+func getItemsWithPrice(value int, menu map[string]int) []string {
+	var items []string
+	for item, price := range menu {
+		if price == value {
+			items = append(items, item)
+		}
+	}
+	return items
+}
+
+func main() {
+	menu := map[string]int{}
+
+	menu["Benih Lele"] = 50000
+	menu["Pakan lele cap menara"] = 25000
+	menu["Probiotik A"] = 75000
+	menu["Probiotik Nila B"] = 10000
+	menu["Pakan Nila"] = 20000
+	menu["Benih Nila A"] = 20000
+	menu["Cupang"] = 5000
+	menu["Benih Nila B"] = 30000
+	menu["Benih Cupang"] = 10000
+	menu["Probiotik B"] = 10000
+
+	fmt.Println("-----------------------------------------------")
+
+	// a
+	keranjang := buyMaxItems(menu, 100000)
+
+	fmt.Println("Total produk dengan harga dibawah Rp 100.000 : ", len(keranjang))
+
+	for _, item := range keranjang {
+		fmt.Println(item, "-", menu[item])
 	}
 
 	fmt.Println("-----------------------------------------------")
 
 	// b
-	fmt.Println("Daftar produk termurah: ", items[0], "-", prices[items[0]])
-	fmt.Println("Daftar produk termahal: ", items[len(items)-1], "-", prices[items[len(items)-1]])
+	fmt.Println("Daftar produk termurah: ", cheapestItem(menu), "-", menu[cheapestItem(menu)])
+	fmt.Println("Daftar produk termahal: ", highestItem(menu), "-", menu[highestItem(menu)])
 
 	fmt.Println("-----------------------------------------------")
 
 	// c
 	fmt.Println("Daftar produk dengan harga Rp 10.000 :	")
-	for item, price := range prices {
-		if price == 10000 {
-			fmt.Println(item, "-", price)
-		}
+
+	items10k := getItemsWithPrice(10000, menu)
+
+	for _, item := range items10k {
+		fmt.Println(item, "-", menu[item])
 	}
 
 	fmt.Println("-----------------------------------------------")
